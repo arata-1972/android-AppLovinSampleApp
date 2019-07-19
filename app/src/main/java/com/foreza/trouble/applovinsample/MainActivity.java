bb4b1e5e5a5cc1108b2349a0aa987244a02e01d4
Jason Chiu <jforeza@gmail.com>
2019-07-19 00:18:32 -0700

    Update banner init to use zone id, move banner to bottom of activity view

--- app/src/main/java/com/foreza/trouble/applovinsample/MainActivity.java
+++ app/src/main/java/com/foreza/trouble/applovinsample/MainActivity.java
@@ -1,13 +1,17 @@
 package com.foreza.trouble.applovinsample;
 
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import com.applovin.adview.AppLovinAdView;
 import com.applovin.sdk.AppLovinAdSize;
 import com.applovin.sdk.AppLovinSdk;
+
+import static android.view.Gravity.BOTTOM;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -37,14 +41,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void initializeAndLoadBanner(){
 
-        adView = new AppLovinAdView( AppLovinAdSize.BANNER, this );
+        adView = new AppLovinAdView( AppLovinAdSize.BANNER, "7d19bb9d8bcac94d",this );
+        adView.setId(ViewCompat.generateViewId());
 
         final ViewGroup rootView = (ViewGroup) findViewById( android.R.id.content );
         rootView.addView( adView );
 
         adView.setLayoutParams( new FrameLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
-                com.applovin.sdk.AppLovinSdkUtils.dpToPx( this, AppLovinAdSize.BANNER.getHeight()) ) );
+                com.applovin.sdk.AppLovinSdkUtils.dpToPx( this, AppLovinAdSize.BANNER.getHeight()), BOTTOM));
 
         adView.loadNextAd();
 
