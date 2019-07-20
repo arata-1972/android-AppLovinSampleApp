2095280a3d37b51e68966e13174efc342f8e62ca
Jason Chiu <jforeza@gmail.com>
2019-07-20 16:10:16 -0700

    Add banner listeners for click, load, and display to adView on banner init

--- app/src/main/java/com/foreza/trouble/applovinsample/MainActivity.java
+++ app/src/main/java/com/foreza/trouble/applovinsample/MainActivity.java
@@ -3,11 +3,17 @@ package com.foreza.trouble.applovinsample;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.applovin.adview.AppLovinAdView;
+import com.applovin.sdk.AppLovinAd;
+import com.applovin.sdk.AppLovinAdClickListener;
+import com.applovin.sdk.AppLovinAdDisplayListener;
+import com.applovin.sdk.AppLovinAdLoadListener;
 import com.applovin.sdk.AppLovinAdSize;
 import com.applovin.sdk.AppLovinSdk;
 
@@ -17,6 +23,7 @@ import static android.view.Gravity.BOTTOM;
 public class MainActivity extends AppCompatActivity {
 
     AppLovinAdView adView;
+    String LOG_TAG = "MainActivity";
 
 
     @Override
@@ -39,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
         // ....
     }
 
+
+    // This method will initialize the adView and attach listeners.
     private void initializeAndLoadBanner(){
 
         adView = new AppLovinAdView( AppLovinAdSize.BANNER, "7d19bb9d8bcac94d",this );
@@ -50,6 +59,51 @@ public class MainActivity extends AppCompatActivity {
         adView.setLayoutParams( new FrameLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 com.applovin.sdk.AppLovinSdkUtils.dpToPx( this, AppLovinAdSize.BANNER.getHeight()), BOTTOM));
+
+
+
+        adView.setAdLoadListener(new AppLovinAdLoadListener() {
+            @Override
+            public void adReceived(AppLovinAd ad) {
+                String msg = "Banner adReceived for: " + ad.getZoneId();
+                Log.d(LOG_TAG, msg);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void failedToReceiveAd(int errorCode) {
+                String msg = "Banner failedToReceiveAd with error: " + errorCode;
+                Log.d(LOG_TAG, msg);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        adView.setAdDisplayListener(new AppLovinAdDisplayListener() {
+            @Override
+            public void adDisplayed(AppLovinAd ad) {
+                String msg = "Banner adDisplayed for: " + ad.getZoneId();
+                Log.d(LOG_TAG, msg);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void adHidden(AppLovinAd ad) {
+                String msg = "Banner adHidden for: " + ad.getZoneId();
+                Log.d(LOG_TAG, msg);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        adView.setAdClickListener(new AppLovinAdClickListener() {
+            @Override
+            public void adClicked(AppLovinAd ad) {
+                String msg = "Banner adClicked for: " + ad.getZoneId();
+                Log.d(LOG_TAG, msg);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         adView.loadNextAd();
 
